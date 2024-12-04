957ed48108e028083025a520d8e2740214c3ad6f
AbdilahCoding <abdilahelaidii@gmail.com>
2024-12-04 12:28:58 +0100

    add new footer

--- header/footer.php
+++ header/footer.php
@@ -9,18 +9,8 @@
                             <a class="navbar-brand logo footer-logo" href="index.html">
             <img class="img-fluid" src="images/logo_white.png" alt="">
           </a>
-<<<<<<< HEAD
                             <p class="mt-4 mb-5">SelfImpro Digital Marketing Agency Template Is fully responsible, Build whatever you like with the Seoland, Seoland is the creative, modern HTML5 Template suitable for Your Business Marketing.</p>
                     
-=======
-                            <p class="mt-4 mb-5">Your go-to resource for practical self-improvement strategies to enhance mental health and well-being. We provide actionable advice, expert insights, and inspiring stories to empower you on your journey toward personal growth, resilience, and happiness. Whether you're seeking mindfulness techniques, productivity hacks, or tools to boost self-confidence, our is here to support and guide you every step of the way.</p>
-                            <!-- <div class="subscribe-form">
-                                <form id="mc-form" class="mc-form">
-                                    <input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required="">
-                                    <input class="subscribe-btn" type="submit" name="subscribe" value="Subscribe Now">
-                                </form>
-                            </div> -->
->>>>>>> bcc4565943557d9b3205fd4662ac330c503e9e5d
                         </div>
                         <div class="col-lg-4 col-md-5 mt-6 mt-md-0">
                             <h5>Contact</h5>
@@ -33,16 +23,11 @@
                                 <li>
                                     <i class="fas fa-envelope"></i>
                                     <h6>Send Us Email</h6>
-                                    <a href="mailto:themeht23@gmail.com">contact@selfimpro.com</a>
+                                    <span>contact@selfimpro.com</span>
                                 </li>
                                 <li>
-<<<<<<< HEAD
                                     <i class="flaticon-mobile"></i>
                                     <h6>Phone</h6>
-=======
-                                    <i class="fas fa-phone"></i>
-                                    <h6>Looking For Project</h6>
->>>>>>> bcc4565943557d9b3205fd4662ac330c503e9e5d
                                     <a href="tel:+912345678900">+91-234-567-8900</a>
                                 </li>
                             </ul>
@@ -63,24 +48,7 @@
                                         </li>
                                     </ul>
                                 </div>
-<<<<<<< HEAD
                                
-=======
-                                <div class="col-md-6 mt-5 mt-md-0 footer-menu">
-                                    <h5>Sales Info</h5>
-                                    <ul class="list-unstyled">
-                                        <li>
-                                            <a href="blog-right-sidebar.html">Blog</a>
-                                        </li>
-                                        <li>
-                                            <a href="privacy-policy.html">Privacy Policy</a>
-                                        </li>
-                                        <li>
-                                            <a href="terms-and-conditions.html">Terms</a>
-                                        </li>
-                                    </ul>
-                                </div>
->>>>>>> bcc4565943557d9b3205fd4662ac330c503e9e5d
                             </div>
                         </div>
                     </div>
@@ -90,15 +58,9 @@
                 <div class="container">
                     <div class="copyright">
                         <div class="row align-items-center">
-<<<<<<< HEAD
                             <div class="col-md-12 text-center">Copyright 2024 SelfImpro 
             </u> | All Rights Reserved </div>
                             
-=======
-                            <div class="col-md-6">Copyright 2024 Selfimpro <u>
-            </u> | All Rights Reserved </div>
-                           
->>>>>>> bcc4565943557d9b3205fd4662ac330c503e9e5d
                         </div>
                     </div>
                 </div>
